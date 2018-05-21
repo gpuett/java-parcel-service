518829e3d42aa611073b6d62c825b1d085ce39c1
Garnett Puett <gpuett@gmail.com>
2018-05-21 15:31:41 -0700

    Add costToShip method

--- src/main/java/models/Parcel.java
+++ src/main/java/models/Parcel.java
@@ -5,19 +5,39 @@ public class Parcel {
     public int width;
     public int length;
     public int weight;
+    public int volume;
 
-    public Parcel(int height, int width, int length, int weight) {
+
+    public Parcel(int height, int width, int length, int weight, int volume) {
         this.height = height;
         this.width = width;
         this.length = length;
         this.weight = weight;
+        this.volume = height * width * length;
+
     }
 
-    public int volume(int height, int width, int length){
-        return (this.height * this.width * this.length);
-    }
-
-    public int costToShip(){
-
+    public int costToShip(String speed){
+        if (speed.equals("Standard")){
+            if (this.volume <= 1000 && this.weight <= 100){
+                return this.volume / 100;
+            } else if (this.volume >= 1000 && this.weight <= 100){
+                return this.volume / 50;
+            } else if (this.volume <= 1000 && this.weight >= 100) {
+                return this.volume / 33;
+            } else{
+                return this.volume / 25;
+            }
+        } else {
+            if (this.volume <= 1000 && this.weight <= 100){
+                return this.volume / 50;
+            } else if (this.volume >= 1000 && this.weight <= 100){
+                return this.volume / 33;
+            } else if (this.volume <= 1000 && this.weight >= 100) {
+                return this.volume / 25;
+            } else{
+                return this.volume / 10;
+            }
+        }
     }
 }
